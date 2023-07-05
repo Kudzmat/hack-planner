89601976149f0a1d249a0465c821e1cd8347d707
Kudz <kudzayibamhare@gmail.com>
2023-07-05 19:01:25 +0200

    updated project

--- app.py
+++ app.py
@@ -1,43 +1,91 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
 items = []
+db_path = 'checklist.db'
+
+
+# create table in database
+def create_table():
+    conn = sqlite3.connect(db_path)  # setting connection
+    c = conn.cursor()  # instantiating cursor
+
+    # specifying the format of the data which will be stored in checklist database
+    c.execute('''CREATE TABLE IF NOT EXISTS checklist
+    (id INTEGER PRIMARY KEY AUTOINCREMENT, item TEXT)''')
+    conn.commit()
+    conn.close()  # close database
+
+
+# select items from database
+def get_items():
+    conn = sqlite3.connect(db_path)  # setting connection
+    c = conn.cursor()  # instantiating cursor
+    c.execute("SELECT * FROM checklist")
+    items = c.fetchall()
+    conn.close()
+    return items
+
+
+def add_item(item):
+    conn = sqlite3.connect(db_path)  # setting connection
+    c = conn.cursor()  # instantiating cursor
+    c.execute("INSERT INTO checklist (item) VALUES (?)", (item,))
+    conn.commit()
+    conn.close()
+
+
+def update_item(item_id, new_item):
+    conn = sqlite3.connect(db_path)  # setting connection
+    c = conn.cursor()  # instantiating cursor
+    c.execute("UPDATE checklist SET item = ? WHERE id = ?", (new_item, item_id))
+    conn.commit()
+    conn.close()
+
+
+def delete_item(item_id):
+    conn = sqlite3.connect(db_path)  # setting connection
+    c = conn.cursor()  # instantiating cursor
+    c.execute("DELETE FROM checklist WHERE id = ?", (item_id,))
 
 
 # create
 @app.route('/add', methods=['POST'])
-def add_item():
+def add_task():
     item = request.form['item']
-    items.append(item)
-
+    add_item(item)  # add item to database
     return redirect('/')
 
 
 # read
 @app.route('/')
 def checklist():
+    create_table()  # create table
+    items = get_items()  # get items from table
     return render_template('checklist.html', items=items)
 
 
 # update endpoint
 @app.route('/edit/<int:item_id>', methods=['GET', 'POST'])
-def edit_item(item_id):
-    item = items[item_id - 1]  # retrieve the item based on its index
-
+def edit_task(item_id):
     if request.method == 'POST':
         new_item = request.form['item']
-        items[item_id - 1] = new_item  # update item with new value
-
+        update_item(item_id, new_item)
         return redirect('/')
 
-    return render_template('edit.html', item=item, item_id=item_id)
+    else:
+        items = get_items()
+        # we want to associate our tasks (items) with their id in case there are duplicate tasks
+        item = next((x[1] for x in items if x[0] == item_id), None)
+        return render_template('edit.html', item=item, item_id=item_id)
 
 
 # delete
 @app.route('/delete/<int:item_id>')
-def delete_item(item_id):
-    del items[item_id - 1]  # delete item at the index
+def delete_task(item_id):
+    delete_item(item_id)  # delete item from database
     return redirect('/')
 
 
